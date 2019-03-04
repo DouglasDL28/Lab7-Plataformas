9c696a755910573cf806ff7c47eed2140799f562
DouglasDL28 <dele18037@uvg.edu.gt>
2019-03-03 21:42:34 -0600

    Agregué código del ejemplo.

---
+++ Notes-App-master/app/src/main/java/com/mogalabs/ejemplo/viewmodels/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package com.mogalabs.ejemplo.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.mogalabs.ejemplo.data.NoteRepository
+import com.mogalabs.ejemplo.data.Note
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+
+    fun update(note: Note) {
+        repository.update(note)
+    }
+
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
